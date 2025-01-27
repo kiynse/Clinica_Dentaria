ae506e596ca75469af55cd98e0147e3f9001fbc1
Hiago Freitas <h07.machado@gmail.com>
2025-01-27 10:05:43 +0000

    Slider

---
+++ frontend/src/Components/Slider.js
@@ -0,0 +1,75 @@
+// src/Components/Slider.js
+import React from 'react';
+import Slider from 'react-slick'; // Importando o Slider da biblioteca react-slick
+
+import '../Style/Slider.css'; // Importando os estilos
+
+// Importando o CSS do slick
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+// Importando as imagens
+import img1 from '../img/Slider1.jpeg'; // Caminho relativo para a imagem
+import img2 from '../img/Slider2.png';
+import img3 from '../img/Slider3.webp';
+import img4 from '../img/Slider4.png';
+
+// Componente do botão de navegação para o "próximo"
+const CustomNextArrow = ({ className, style, onClick }) => {
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: 'block', background: '#03b1b4', borderRadius: '50%' }}
+      onClick={onClick}
+    >
+      ➡
+    </div>
+  );
+};
+
+// Componente do botão de navegação para o "anterior"
+const CustomPrevArrow = ({ className, style, onClick }) => {
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: 'block', background: '#03b1b4', borderRadius: '50%' }}
+      onClick={onClick}
+    >
+      ⬅
+    </div>
+  );
+};
+
+function SliderComponent() {
+  // Definindo as configurações do slider
+  const settings = {
+    dots: true, // Adiciona os pontos de navegação abaixo
+    infinite: true, // Habilita navegação infinita
+    speed: 500, // Velocidade de transição
+    slidesToShow: 1, // Exibe 1 slide por vez
+    slidesToScroll: 1, // Navega 1 slide por vez
+    nextArrow: <CustomNextArrow />, // Customizando o botão "próximo"
+    prevArrow: <CustomPrevArrow />, // Customizando o botão "anterior"
+  };
+
+  return (
+    <div className="slider-container">
+      <Slider {...settings}>
+        <div>
+          <img src={img1} alt="Slide 1" />
+        </div>
+        <div>
+          <img src={img2} alt="Slide 2" />
+        </div>
+        <div>
+          <img src={img3} alt="Slide 3" />
+        </div>
+        <div>
+          <img src={img4} alt="Slide 4" />
+        </div>
+      </Slider>
+    </div>
+  );
+}
+
+export default SliderComponent;
--- frontend/src/View/Home.js
+++ frontend/src/View/Home.js
@@ -1,11 +1,15 @@
 // src/View/Home.js
 import React from 'react';
 import Navbar from '../Components/Navbar'; // Importando o componente Navbar
+import SliderComponent from '../Components/Slider'; // Importando o Slider
 
 function Home() {
   return (
     <div>
       <Navbar /> {/* Adicionando a Navbar no topo da página */}
+      
+      <SliderComponent /> {/* Adicionando o Slider na Home */}
+
       <h1>Bem-vindo à Home!</h1>
       <p>Esta é a sua página inicial.</p>
     </div>
